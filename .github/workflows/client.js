2ce05baeabd24d147309c3c6753684c0c393bc1e
gggho0806 <gggho0806@gmail.com>
2021-10-31 21:50:21 -0400

    add new workflow

---
+++ .github/workflows/client.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
+
+pgclient.connect();
+
+const table = 'CREATE TABLE public.tokens ( discord_id varchar(100) NOT NULL, "token" text NULL, expirydatetime timestamp NULL, "scope" varchar NULL, caltype varchar NULL, calid varchar NULL, cal_id varchar(255) NULL, cal_type varchar(255) NULL, code varchar(255) NULL, expires timestamp NULL, "refresh" varchar(255) NULL, CONSTRAINT tokens_pkey PRIMARY KEY (discord_id) '
+pgclient.query(table, (err, res) => {
+    if (err) throw err
+});
