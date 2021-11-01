cce6f0bfa19bb5d7b7a1be41ec0d279f183c0bf5
gggho0806 <gggho0806@gmail.com>
2021-11-01 00:05:34 -0400

    fix the sql error in init script

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const pgclient = new Client({
 
 pgclient.connect();
 
-const table = 'CREATE TABLE public.tokens ( discord_id varchar(100) NOT NULL, "token" text NULL, expirydatetime timestamp NULL, "scope" varchar NULL, caltype varchar NULL, calid varchar NULL, cal_id varchar(255) NULL, cal_type varchar(255) NULL, code varchar(255) NULL, expires timestamp NULL, "refresh" varchar(255) NULL, CONSTRAINT tokens_pkey PRIMARY KEY (discord_id) '
+const table = 'CREATE TABLE public.tokens ( discord_id varchar(100) NOT NULL , "token" text NULL, expirydatetime timestamp NULL, "scope" varchar NULL, caltype varchar NULL, calid varchar NULL, cal_id varchar(255) NULL, cal_type varchar(255) NULL, code varchar(255) NULL, expires timestamp NULL, "refresh" varchar(255) NULL, CONSTRAINT tokens_pkey PRIMARY KEY (discord_id)) '
 pgclient.query(table, (err, res) => {
     if (err) {
         console.log(err);
