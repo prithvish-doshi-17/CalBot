aca3aea103679e6f67ce937bbd64941b8905db0b
Seeyapm <siyapm@gmail.com>
2021-11-03 22:15:18 -0400

    Updated a few comments

--- src/main/java/com/se21/calbot/interfaces/Calendar.java
+++ src/main/java/com/se21/calbot/interfaces/Calendar.java
@@ -10,7 +10,7 @@ public interface Calendar {
 
     /**
      * It serves any authenticate request
-     * @param id User unique identifier
+     * @param id: User unique identifier
      * @return URL for authentication
      */
     String authenticate(String id);
@@ -18,15 +18,15 @@ public interface Calendar {
     /**
      * Saves received access token to db and perform some basic functionalities like getting
      * access token and refresh token further.
-     * @param code auth code
-     * @param id user Id
+     * @param code: auth code
+     * @param id: user Id
      * @throws Exception generic exception
      */
     void saveAccessToken(String code, String id) throws Exception;
 
     /**
      * Retrieves event from calendar
-     * @param calId calendar id(One google calendar can have multiple calendars at same time)
+     * @param calId: calendar id(One google calendar can have multiple calendars at same time)
      * @return JSON objects of calendar returned events
      * @throws Exception generic exception
      */
@@ -34,7 +34,7 @@ public interface Calendar {
 
     /**
      * Updates existing events to calendar
-     * @param req type is JSON object, exact parameters not yet decided, but can be changed based on different type of calendars
+     * @param req: type is JSON object, exact parameters not yet decided, but can be changed based on different type of calendars
      * @return response from calendar
      */
     Enums.calApiResponse updateEvents(JSONObject req);
@@ -42,19 +42,21 @@ public interface Calendar {
     /**
      * Adds event to calendar, currently you can only add unscheduled
      * events i.e. when there is no start date but only deadline and number of hours need to be dedicated
-     * @param title Event title
-     * @param hours Number of hours to be dedicated
-     * @param deadline deadline for this activity
+     * @param title: Event title
+     * @param hours: Number of hours to be dedicated
+     * @param deadline: deadline for this activity
      * @return Calendar response
      */
     Enums.calApiResponse addEvents(String title, String hours, String deadline) throws Exception;
 
+    
+    Enums.calApiResponse deleteEvents();
+    
     /**
      * Deletes one or some event
+     * @param title: Event title
      * @return Calendar response
      */
-    Enums.calApiResponse deleteEvents();
-
     Enums.calApiResponse deleteEvents(String eventId);
 
     /**
